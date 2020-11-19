918e209684c0fe1d2e149a6f50cb2f035fca07ae
neaar3 <neaar3@gmail.com>
2020-11-19 03:02:30 -0300

    refactor: added new functions in client's logic  controller

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -1,9 +1,24 @@
 const {response} = require('../utils/response');
-const Clientes = require('../repositories/clientes')
+const Clientes = require('../repositories/clientes');
+const Auth = require('./auth');
+
+
+const criarCliente = async (ctx) => {
+
+    const {nome = null, cpf = null, email = null, telefone = null, id = null} = ctx.request.body;
+
+    if(!nome || !cpf || !email || !telefone || !id) {
+        return response(ctx, 400, 'Favor preencher todos os dados')
+    }
+    const result = await Clientes.criarNovoCliente(nome, cpf, email, telefone, id);
+    return response(ctx, 201, {id: `${result.id}`})
+    
+}
+
 
 const todosOsClientes = async (ctx) => {
     const result = await Clientes.listarClientes();
     return response(ctx, 200, result)
 }
 
-module.exports = { todosOsClientes };
+module.exports = { todosOsClientes, criarCliente };
