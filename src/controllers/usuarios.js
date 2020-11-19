a99203032a095783cc08449ec63d3d8496221245
neaar3 <neaar3@gmail.com>
2020-11-19 03:03:06 -0300

    refactor: added new functions in user's logic controller

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,12 +1,17 @@
 const {response} = require('../utils/response');
 const Usuarios = require('../repositories/usuarios')
+const Senha = require('../utils/senha');
 
 const criarUsuario = async (ctx) => {
-    await Usuarios.criarTabelaDeUsuarios();
-    const dados = ctx.request.body;
-    const result = await Usuarios.criarNovoUsuario(dados.email, dados.senha, dados.nome);
 
-    return response(ctx, 200, result)
+    const {nome = null, email = null, senha = null} = ctx.request.body;
+    if(!nome || !email || !senha) {
+        return response(ctx, 400, 'Favor preencher todos os dados')
+    }
+    const senhaEncriptada = await Senha.encrypt(senha);
+    const result = await Usuarios.criarNovoUsuario(email, senhaEncriptada, nome);
+
+    return response(ctx, 201, {id: `${result.id}`})
 }
 
 const todosOsUsuarios = async (ctx) => {
