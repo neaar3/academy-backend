296738220a7d1c029384108ece4cdd3df406448b
neaar3 <neaar3@gmail.com>
2020-11-19 02:59:12 -0300

    refactor: new routes added.

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,11 @@ const Session = require('./middlewares/session');
 const Clientes = require('./controllers/clientes');
 const Usuarios = require('./controllers/usuarios')
 
+router.post("/auth", Auth.autenticar)
 router.post("/usuarios", Usuarios.criarUsuario);
 router.get("/usuarios", Usuarios.todosOsUsuarios);
-
-router.get("/clientes", Clientes.todosOsClientes);
+router.post("/clientes", Session.verify, Clientes.criarCliente)
+router.get("/clientes", Session.verify, Clientes.todosOsClientes);
 
 
 module.exports = router;
